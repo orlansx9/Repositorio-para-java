e3aed25beecf1562d8e30c3aa38a54ca054911db
orlansx9 <orlando-k9999@hotmail.com>
2021-03-19 18:33:50 -0600

    conceptosbasicos

---
+++ src/conceptosbasicos/Conceptosbasicos.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conceptosbasicos;
+
+import ico.fes.Automovil;
+import java.awt.Color;
+
+/**
+ *
+ * @author Orlando
+ */
+public class Conceptosbasicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Automovil miBocho= new Automovil();
+        miBocho.setMarca("VW");
+        miBocho.setSubMarca("Sedan");
+        miBocho.setModelo(1970);
+        miBocho.setColor(Color.BLUE);
+        System.out.println(miBocho);
+        
+        //programar los objetos mi acura y mi mustang
+    }
+    
+}
